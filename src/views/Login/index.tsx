f4e44c3d161fc1c3defbb2cfab624f48c4e8bafb
Kyle Willard <kyleswillard@gmail.com>
2021-02-13 22:54:17 -0800

    added state to login page to push to action creator, does not dispatch

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,9 +1,22 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
+
 import Input from '../../components/custom_styled_components/Input';
 
 interface LoginProps {}
 
 const Login: React.FC<LoginProps> = () => {
+	const [credentials, setCredentials] = useState({
+		username: '',
+		password: ''
+	});
+
+	const onChange = event => {
+		setCredentials({
+			...credentials,
+			[event.target.name]: event.target.value
+		});
+	};
+
 	const onSubmit = (e: FormEvent) => {
 		e.preventDefault();
 	};
@@ -11,12 +24,20 @@ const Login: React.FC<LoginProps> = () => {
 		<div>
 			<h1>Login</h1>
 			<form onSubmit={onSubmit}>
-				<Input id="username" name="username" label="Username" />
+				<Input
+					id="username"
+					name="username"
+					label="Username"
+					value={credentials.username}
+					onChange={onChange}
+				/>
 				<Input
 					id="password"
 					name="password"
 					label="Password"
 					type="password"
+					value={credentials.password}
+					onChange={onChange}
 				/>
 				<button type="submit">Login</button>
 			</form>
