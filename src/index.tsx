c10cc870a172f5363f17e8fedbaedafac0519657
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-13 20:12:02 -0500

    Added a few new things

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
-import './indexStyle.scss';
+import './scss/indexStyle.scss';
 
 const apolloClient = new ApolloClient({
 	uri: 'https://bolt--backend.herokuapp.com/graphql',
