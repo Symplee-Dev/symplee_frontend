6d037c8e98b50fef252da788de918de650494ae7
Kyle Willard <kyleswillard@gmail.com>
2021-02-13 23:14:36 -0800

    added state to register

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,18 +1,19 @@
 import React, { FormEvent, useState } from 'react';
+// import styled from 'styled-components';
 
 import Input from '../../components/custom_styled_components/Input';
 
 interface LoginProps {}
 
 const Login: React.FC<LoginProps> = () => {
-	const [credentials, setCredentials] = useState({
+	const [loginCredentials, setLoginCredentials] = useState({
 		username: '',
 		password: ''
 	});
 
 	const onChange = event => {
-		setCredentials({
-			...credentials,
+		setLoginCredentials({
+			...loginCredentials,
 			[event.target.name]: event.target.value
 		});
 	};
@@ -28,7 +29,7 @@ const Login: React.FC<LoginProps> = () => {
 					id="username"
 					name="username"
 					label="Username"
-					value={credentials.username}
+					value={loginCredentials.username}
 					onChange={onChange}
 				/>
 				<Input
@@ -36,7 +37,7 @@ const Login: React.FC<LoginProps> = () => {
 					name="password"
 					label="Password"
 					type="password"
-					value={credentials.password}
+					value={loginCredentials.password}
 					onChange={onChange}
 				/>
 				<button type="submit">Login</button>
