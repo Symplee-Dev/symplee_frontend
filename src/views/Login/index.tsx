0cbf8b35e5e673e733ab6a818d257d4062384507
Nathaniel Richards <nthnlrichards@gmail.com>
2021-02-15 00:30:23 -0500

    Working on Login

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -4,6 +4,7 @@ import './style.scss';
 import { motion } from 'framer-motion';
 import NavBar from '../../components/NavBar';
 import FadeIn from 'react-fade-in';
+import { MenuItem, Select, TextField } from '@material-ui/core';
 
 interface LoginProps {}
 
@@ -11,7 +12,7 @@ const Login: React.FC<LoginProps> = () => {
 	const [loginCredentials, setLoginCredentials] = useState({
 		username: '',
 		password: '',
-		email: ''
+		key: ''
 	});
 
 	const [usernameType, setUsernameType] = useState<'USERNAME' | 'EMAIL'>(
@@ -29,6 +30,7 @@ const Login: React.FC<LoginProps> = () => {
 	const onSubmit = (e: FormEvent) => {
 		e.preventDefault();
 	};
+
 	return (
 		<>
 			<NavBar />
@@ -38,9 +40,72 @@ const Login: React.FC<LoginProps> = () => {
 						<h3>Welcome Back!</h3>
 						<form onSubmit={onSubmit}>
 							{/* Switch component = using username or email */}
+							<p className="input-title">Login With</p>
+							<Select
+								value={usernameType}
+								className="username-select"
+								onChange={e => {
+									setLoginCredentials({
+										...loginCredentials,
+										username: ''
+									});
+									setUsernameType(
+										e.target.value as 'EMAIL' | 'USERNAME'
+									);
+								}}
+							>
+								<MenuItem value="EMAIL">Email</MenuItem>
+								<MenuItem value="USERNAME">Username</MenuItem>
+							</Select>
 							{usernameType === 'EMAIL' && (
-								<p className="input-title">Email</p>
+								<>
+									<p className="input-title">Email</p>
+									<TextField
+										name="username"
+										variant="filled"
+										className="input-field"
+										type="email"
+										value={loginCredentials.username}
+										onChange={onChange}
+									/>
+								</>
 							)}
+
+							{usernameType === 'USERNAME' && (
+								<>
+									<p className="input-title">
+										Username And Key
+									</p>
+
+									<div className="username-input-group">
+										<TextField
+											name="username"
+											variant="filled"
+											className="input-field"
+											type="text"
+											value={loginCredentials.username}
+											onChange={onChange}
+										/>
+										<TextField
+											name="key"
+											className="key-field"
+											variant="filled"
+											value={loginCredentials.key}
+											onChange={onChange}
+										/>
+									</div>
+								</>
+							)}
+
+							<p className="input-title">Password</p>
+							<TextField
+								name="password"
+								variant="filled"
+								className="input-field"
+								type="password"
+								value={loginCredentials.password}
+								onChange={onChange}
+							/>
 
 							{/* <Input
 							id="username"
