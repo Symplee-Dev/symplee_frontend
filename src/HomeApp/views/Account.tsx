0802ee227a022a08a9f1821e71632c48538f8221
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-18 01:50:42 -0500

    Finishing up account

--- src/HomeApp/views/Account.tsx
+++ src/HomeApp/views/Account.tsx
@@ -7,13 +7,14 @@ import randomHex from 'random-hex';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import Moment from 'react-moment';
 import { useState, useEffect } from 'react';
+import UndoIcon from '@material-ui/icons/Undo';
 
 const Account = ({ user }: { user: User }) => {
 	const [formState, setFormState] = useState<{
 		name: string;
 		username: string;
 		email: string;
-	}>();
+	}>({ name: '', username: '', email: '' });
 
 	useEffect(() => {
 		setFormState({
@@ -57,11 +58,114 @@ const Account = ({ user }: { user: User }) => {
 						ago
 					</div>
 				</div>
+
 				<div className="top-bottom">
 					<h2>Profile Details</h2>
+
 					<div className="row">
-						<p>Name</p>
-						<TextField color="primary" value={formState?.name} />
+						<div>
+							<p>Name </p>
+							{user.name === formState.name ? (
+								''
+							) : (
+								<Tooltip placement="top" title="Undo">
+									<UndoIcon
+										className="undo"
+										style={{ color: '#F2A813' }}
+										onClick={() =>
+											setFormState({
+												...formState,
+												name: user.name
+											})
+										}
+									/>
+								</Tooltip>
+							)}
+						</div>
+						<TextField
+							color="primary"
+							style={{ width: '100%' }}
+							onChange={e =>
+								setFormState({
+									...formState,
+									name: e.target.value
+								})
+							}
+							variant="filled"
+							value={formState?.name}
+							inputProps={{ className: 'input-field' }}
+						/>
+					</div>
+					<div className="row">
+						<div>
+							<p>Email</p>
+							{user.email === formState.email ? (
+								''
+							) : (
+								<Tooltip placement="top" title="Undo">
+									<UndoIcon
+										className="undo"
+										style={{ color: '#F2A813' }}
+										onClick={() =>
+											setFormState({
+												...formState,
+												email: user.email
+											})
+										}
+									/>
+								</Tooltip>
+							)}
+						</div>
+						<TextField
+							color="primary"
+							style={{ width: '100%' }}
+							variant="filled"
+							value={formState?.email}
+							onChange={e =>
+								setFormState({
+									...formState,
+									email: e.target.value
+								})
+							}
+							inputProps={{ className: 'input-field' }}
+						/>
+					</div>
+					<div className="row">
+						<div>
+							<p>Username</p>
+							{user.username === formState.username ? (
+								''
+							) : (
+								<Tooltip placement="top" title="Undo">
+									<UndoIcon
+										className="undo"
+										style={{ color: '#F2A813' }}
+										onClick={() =>
+											setFormState({
+												...formState,
+												username: user.username
+											})
+										}
+									/>
+								</Tooltip>
+							)}
+						</div>
+						<TextField
+							style={{ width: '100%' }}
+							onChange={e =>
+								setFormState({
+									...formState,
+									username: e.target.value
+								})
+							}
+							color="primary"
+							variant="filled"
+							value={formState?.username}
+							inputProps={{ className: 'input-field' }}
+						/>
+					</div>
+					<div className="row">
+						<button>Save</button>
 					</div>
 				</div>
 			</div>
