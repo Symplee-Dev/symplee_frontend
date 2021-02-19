9c7eac2a38e8a832dfef2f11a9623e454b6bb7af
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-19 15:39:40 -0500

    Create chat group done

--- src/HomeApp/views/CreateGroup.tsx
+++ src/HomeApp/views/CreateGroup.tsx
@@ -4,13 +4,35 @@ import { motion } from 'framer-motion';
 import CheckIcon from '@material-ui/icons/Check';
 
 import './createGroup.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Avatar, LinearProgress, TextField } from '@material-ui/core';
+import { useCreateChatGroupMutation } from '../../graphql';
+import { useSelector, RootStateOrAny } from 'react-redux';
+import { useHistory } from 'react-router';
 
 const CreateGroup = () => {
 	const [newGroup, setNewGroup] = useState({
 		isPublic: false,
 		name: ''
 	});
+
+	const history = useHistory();
+
+	const [create, { data, loading, error }] = useCreateChatGroupMutation();
+
+	const userId = useSelector((state: RootStateOrAny) => state.user.userId);
+
+	useEffect(() => {
+		if (data && !loading && !error) {
+			history.push('/group/' + data.createChatGroup.id);
+		}
+	}, [data, error, history, loading]);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		create({ variables: { chatGroup: { ...newGroup, userId } } });
+	};
 
 	return (
 		<motion.div exit={{ opacity: 0 }} className="create-group">
@@ -20,32 +42,63 @@ const CreateGroup = () => {
 				</div>
 				<p>Create a new group.</p>
 			</div>
-			<form>
-				<div className="body"></div>
-				<div className="settings">
-					<div className="top">
-						<h2>Public</h2>
-						<p>
-							{!newGroup.isPublic
-								? 'This group is currently set to private and will not be able to be found on the community page'
-								: 'This group is currently set to public and will be able to be found on the community page'}
-						</p>
-						<ToggleButton
-							value={newGroup.isPublic}
-							selected={newGroup.isPublic}
-							onChange={() =>
+			{loading && <LinearProgress color="primary" />}
+			{!loading && (
+				<form onSubmit={handleSubmit}>
+					<div className="body">
+						<div className="top-body">
+							<Avatar className="avatar">
+								{newGroup.name[0]}
+							</Avatar>
+							<h2>
+								{newGroup.name.length <= 0
+									? 'Untitled Group'
+									: newGroup.name}
+							</h2>
+							<p>
+								{newGroup.isPublic ? '(Public)' : '(Private)'}
+							</p>
+						</div>
+						<TextField
+							fullWidth
+							variant="filled"
+							placeholder="Group Name"
+							color="primary"
+							inputProps={{ className: 'name-field' }}
+							value={newGroup.name}
+							onChange={e =>
 								setNewGroup({
 									...newGroup,
-									isPublic: !newGroup.isPublic
+									name: e.target.value
 								})
 							}
-						>
-							<CheckIcon />
-						</ToggleButton>
+						/>
 					</div>
-					<button>Create Group</button>
-				</div>
-			</form>
+					<div className="settings">
+						<div className="top">
+							<h2>Group Settings</h2>
+							<p>
+								{!newGroup.isPublic
+									? 'This group is currently set to private and will not be able to be found on the community page'
+									: 'This group is currently set to public and will be able to be found on the community page'}
+							</p>
+							<ToggleButton
+								value={newGroup.isPublic}
+								selected={newGroup.isPublic}
+								onChange={() =>
+									setNewGroup({
+										...newGroup,
+										isPublic: !newGroup.isPublic
+									})
+								}
+							>
+								<CheckIcon />
+							</ToggleButton>
+						</div>
+						<button className="create-btn">Create Group</button>
+					</div>
+				</form>
+			)}
 		</motion.div>
 	);
 };
