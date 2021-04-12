c6acea83a4475962b664ccee3f0d18c3153b2246
Nathaniel Richards <nrichardsbusiness@gmail.com>
2021-04-12 01:16:07 -0400

    Almost done with notifcations popover

--- src/V1.0.5/NavSidebar/NotificationPopout/index.tsx
+++ src/V1.0.5/NavSidebar/NotificationPopout/index.tsx
@@ -1,12 +1,138 @@
 import './style.scss';
-import { Popover } from '@material-ui/core';
+import { Popover, Tooltip } from '@material-ui/core';
+import {
+	useGetNotificationsQuery,
+	useAcceptFriendMutation,
+	useDeclineFriendMutation,
+	useAcceptInviteMutation,
+	useDeclineInviteMutation
+} from '../../../graphql';
+import { UserSelectors } from '../../../redux/selectors';
+import CancelSharpIcon from '@material-ui/icons/CancelSharp';
+import CheckCircleSharpIcon from '@material-ui/icons/CheckCircleSharp';
+import Moment from 'react-moment';
+import { useSelector } from 'react-redux';
+import { UserActions, UIActions } from '../../../redux/actions/index';
+import { RootState } from '../../../redux/types/state-types';
+import { Maybe } from '../../../graphql';
 
 export interface PopoutProps {
 	anchor: any;
 	setAnchor: React.Dispatch<any>;
 }
 
+type Notif = Maybe<{
+	id: number;
+	description: string;
+	type?: Maybe<string>;
+	createdAt: string;
+	read: boolean;
+	code?: Maybe<string>;
+	from?: Maybe<{
+		username: string;
+		key: string;
+		id: number;
+	}>;
+}>;
+
 const NotificationPopout = ({ anchor, setAnchor }: PopoutProps) => {
+	const userId = UserSelectors.useSelectUserId();
+
+	const { data, loading, error, refetch } = useGetNotificationsQuery({
+		skip: !userId,
+		variables: { type: 'ALL', userId: userId! }
+	});
+
+	const [acceptFriend] = useAcceptFriendMutation();
+	const [declineFriend] = useDeclineFriendMutation();
+	const [acceptGroup] = useAcceptInviteMutation();
+	const [declineGroup] = useDeclineInviteMutation();
+	const refetchGroups = UserActions.useRefetchUser();
+
+	const addNotification = UIActions.useAddNotification();
+	const notifId = useSelector(
+		(state: RootState) => state.ui.notifications.length + 1
+	);
+
+	const handleAcceptGroup = (notif?: Notif) => {
+		acceptGroup({
+			variables: {
+				acceptArgs: {
+					code: notif?.code!,
+					notificationId: notif?.id!,
+					userId: userId!
+				}
+			}
+		}).then(() => {
+			refetchGroups();
+
+			addNotification({
+				title: 'You have accepted an invite to join the group',
+				id: notifId,
+				type: 'success',
+				autoDismiss: true,
+				autoTimeoutTime: 3000
+			});
+		});
+	};
+
+	const handleDeclineGroup = (notif?: Notif) => {
+		declineGroup({
+			variables: {
+				declineArgs: {
+					code: notif?.code!,
+					notificationId: notif?.id!,
+					userId: userId!
+				}
+			}
+		}).then(() => {
+			addNotification({
+				title: 'You have declined an invite to join the group',
+				id: notifId,
+				type: 'error',
+				autoDismiss: true,
+				autoTimeoutTime: 3000
+			});
+		});
+	};
+
+	const handleAccept = (notif?: Notif) => {
+		acceptFriend({
+			variables: {
+				notificationId: notif?.id!,
+				invite: { fromId: notif?.from?.id!, userId: userId! }
+			}
+		}).then(() => refetch());
+
+		addNotification({
+			title: 'You are now friends with ' + notif?.from?.username,
+			id: notifId,
+			type: 'success',
+			autoDismiss: true,
+			autoTimeoutTime: 3000
+		});
+	};
+
+	const handleDecline = (notif?: Notif) => {
+		declineFriend({
+			variables: {
+				notificationId: notif?.id!,
+				invite: { fromId: notif?.from?.id!, userId: userId! }
+			}
+		}).then(() => refetch());
+
+		addNotification({
+			title:
+				'You have declined ' +
+				notif?.from?.username +
+				"'s friend request",
+			id: notifId,
+			type: 'error',
+			autoDismiss: true,
+			autoTimeoutTime: 3000
+		});
+	};
+
 	return (
 		<Popover
 			style={{ marginLeft: '2rem' }}
@@ -22,7 +148,95 @@ const NotificationPopout = ({ anchor, setAnchor }: PopoutProps) => {
 				horizontal: 'left'
 			}}
 		>
-			Notifications
+			<div className="notif-popover">
+				<div className="notifs">
+					{data && data.getNotifications.length > 1
+						? data.getNotifications
+								.map((notif, key) => (
+									<div className="notif" key={key}>
+										<div className="left">
+											<p className="desc">
+												{notif?.description}
+											</p>
+											<p className="sub">
+												{notif?.read
+													? 'Read'
+													: 'Unread'}
+											</p>
+											<p className="sub">
+												<Moment fromNow>
+													{notif?.createdAt}
+												</Moment>
+											</p>
+										</div>
+
+										<div className="right">
+											{!notif?.read &&
+												notif?.type ===
+													'FRIEND_REQUEST' && (
+													<>
+														<Tooltip
+															placement="top"
+															title="Decline"
+														>
+															<CancelSharpIcon
+																onClick={() =>
+																	handleDecline(
+																		notif
+																	)
+																}
+															/>
+														</Tooltip>
+														<Tooltip
+															placement="top"
+															title="Accept"
+														>
+															<CheckCircleSharpIcon
+																onClick={() =>
+																	handleAccept(
+																		notif
+																	)
+																}
+															/>
+														</Tooltip>
+													</>
+												)}
+											{!notif?.read &&
+												notif?.type === 'INVITE' && (
+													<>
+														<Tooltip
+															placement="top"
+															title="Decline"
+														>
+															<CancelSharpIcon
+																onClick={() =>
+																	handleDeclineGroup(
+																		notif
+																	)
+																}
+															/>
+														</Tooltip>
+														<Tooltip
+															placement="top"
+															title="Accept"
+														>
+															<CheckCircleSharpIcon
+																onClick={() =>
+																	handleAcceptGroup(
+																		notif
+																	)
+																}
+															/>
+														</Tooltip>
+													</>
+												)}
+										</div>
+									</div>
+								))
+								.reverse()
+						: null}
+				</div>
+			</div>
 		</Popover>
 	);
 };
