6bb6f6c6b6f88852bb2dac716b1ed39384b5c15a
Nathaniel Richards <nrichardsbusiness@gmail.com>
2021-04-13 13:44:10 -0400

    Created switch command

--- src/V1.0.5/Chat/ChatGroupInfoSwitch/ChatGroupInfoSwitch.tsx
+++ src/V1.0.5/Chat/ChatGroupInfoSwitch/ChatGroupInfoSwitch.tsx
@@ -4,8 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRandom } from '@fortawesome/free-solid-svg-icons';
 
 import './style.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/types/state-types';
+import Button from '../../components/Button/Button';
+import { Avatar } from '../../components';
 
 const ChatGroupInfoSwitch = ({ anchor, setAnchor }: PopoutProps) => {
+	const groups = useSelector((state: RootState) => state.user.user?.chatGroups);
+
 	return (
 		<Popover
 			style={{ marginLeft: '2rem' }}
@@ -21,9 +27,35 @@ const ChatGroupInfoSwitch = ({ anchor, setAnchor }: PopoutProps) => {
 				horizontal: 'left'
 			}}
 		>
-			<div className="header">
-				<p>Switch Channels</p>
-				<FontAwesomeIcon icon={faRandom} />
+			<div className="popout">
+				<div className="header">
+					<p>Switch Channels</p>
+					<FontAwesomeIcon icon={faRandom} />
+				</div>
+				<div className="groups">
+					{groups?.map((group, key) => (
+						<>
+							<div className="group" key={key}>
+								<div>
+									<Avatar
+										className="small"
+										src={group.avatar ?? ''}
+										hasStatus={false}
+										fallback={group.name[0]}
+									/>
+									<h4>{group.name}</h4>
+								</div>
+								<Button
+									clickHandler={() => alert('Not implemented')}
+									content="Jump"
+									color="main"
+									size="small"
+								/>
+							</div>
+							<hr />
+						</>
+					))}
+				</div>
 			</div>
 		</Popover>
 	);
